68326af7b58f963b003414534bd90d4f2f3ea45a
Daniela Cristancho <50670129+Ivondaniela1607@users.noreply.github.com>
2025-04-27 20:50:43 -0500

    Auto-generated commit

--- chunk-4I4CICT4.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-EQDQRRRY.js";var n=[{path:"",loadComponent:()=>import("./chunk-FTPXNCJN.js").then(e=>e.ManagmentComponent)}];export{n as routes};
---
+++ chunk-YEX5CTI5.js
@@ -0,0 +1,1 @@
+import"./chunk-EQDQRRRY.js";var n=[{path:"",loadComponent:()=>import("./chunk-S2BRJ575.js").then(e=>e.ManagmentComponent)}];export{n as routes};
